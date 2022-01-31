99788e122d066b981de65c6d5288f781ae404dba
Matt Dorsey <matt.dorsey.tx@gmail.com>
2022-01-31 13:57:24 -0600

    changed name of app to angular-seed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'raffle-nft-dapp'`, () => {
+  it(`should have as title 'angular-seed'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('raffle-nft-dapp');
+    expect(app.title).toEqual('angular-seed');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('raffle-nft-dapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-seed app is running!');
   });
 });
